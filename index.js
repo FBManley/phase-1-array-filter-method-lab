cc2466e8a6fe2aa74b4044a2f991c1972835a145
Jacob Manley <jbmanley45@gmail.com>
2022-03-05 12:51:37 -0800

    commit tree fa49902a28b4f56c7298ca16510c1464482bc732

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function (driver) {
+       return driver.toLowerCase() === name.toLowerCase()
+   })
+}
+
+function fuzzyMatch(drivers, que) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase().substring(0, que.length) === que.toLowerCase() 
+    }) 
+}
+
+function matchName(drivers, no){
+    return drivers.filter(function (driver) {
+        return driver.name === no
+    })
+}
